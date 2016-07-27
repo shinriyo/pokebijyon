37802ed015dee0ca45e56a3cbdd51317427571f4
shinsugita <shinsugita@crooz.co.jp>
2016-07-27 14:58:46 +0900

    afi pre

--- myscript.js
+++ myscript.js
@@ -35,17 +35,9 @@ $(function () {
   // 違ったらしない
   if(res == null) return;
 
-  // デバッグツールの人を選ぶ
-  var changeUser = function(num){
-    document.getElementsByName('user_id')[0].value = num;
-  };
-
-  // テキストボックス生成
-  var newTextBoxDiv = $(document.createElement('div'))
-	     .attr("id", 'TextBoxDiv');
-  newTextBoxDiv.after().html('<label>選びたいユーザID</label>' +
-	      '<input type="text" name="userId" id="mynum" value="">'); 
-  newTextBoxDiv.appendTo(".center-align-text");
+  // アフィリエイト
+  var amazon = "<iframe src=\"http://rcm-fe.amazon-adsystem.com/e/cm?t=noctushinrsdi-22&o=9&p=8&l=as1&asins=B01IIGUB4M&ref=tf_til&fc1=000000&IS2=1&lt1=_blank&m=amazon&lc1=0000FF&bc1=000000&bg1=FFFFFF&f=ifr\" style=\"width:120px;height:240px;\" scrolling=\"no\" marginwidth=\"0\" marginheight=\"0\" frameborder=\"0\"></iframe>";
+  //$('.header-map-search-wrapper').after().html(amazon);
 
   // ボタン生成
   $('<button/>', {
